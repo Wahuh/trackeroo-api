59b038217c56efb4d709268e79077e1d3c6b7a67
Thanh <tmdoan98@gmail.com>
2019-10-03 14:34:21 +0100

    add login lambda

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from chalice import Chalice, Response, BadRequestError, ChaliceViewError
-from chalicelib.auth import signup, authorizer
+from chalicelib.auth import signup, authorizer, login
 
 app = Chalice(app_name="trackeroo-api")
 
@@ -18,6 +18,24 @@ def post_signup():
         jwt = signup(username=username, password=password)
         return Response(
             body={"message": "Registration success"},
+            status_code=200,
+            headers={"Authorization": jwt},
+        )
+    except KeyError:
+        raise BadRequestError("Username or password is missing")
+    except Exception as e:
+        raise ChaliceViewError(e)
+
+
+@app.route("/login", cors=True, methods=["POST"])
+def post_login():
+    try:
+        body = app.current_request.json_body
+        password = body["password"]
+        username = body["username"]
+        jwt = login(username=username, password=password)
+        return Response(
+            body={"message": "Login success"},
             status_code=200,
             headers={"Authorization": jwt},
         )
