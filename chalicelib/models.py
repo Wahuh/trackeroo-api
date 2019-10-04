d3c3902ff0053dc6aee768fea7d0eecba09164e8
timotillydo <northcoders5@northcoders5s-MacBook-Pro.local>
2019-10-04 13:01:57 +0100

    added run class with add_one method

--- chalicelib/models.py
+++ chalicelib/models.py
@@ -1,9 +1,33 @@
+import boto3
 from .db import connection
+import uuid
 
-_table = connection.Table("users")
+_users_table = connection.Table("users")
+_runs_table = connection.Table('runs')
+
+dynamodb_client = boto3.client("dynamodb")
+dynamodb_resource = boto3.resource("dynamodb")
 
 
 class User:
     @staticmethod
     def add_one(username):
-        _table.put_item(Item={"username": username})
+        _users_table.put_item(Item={"username": username})
+
+
+class Run:
+    @staticmethod
+    def add_one(
+        username,
+        start_time
+    ):
+        new_run_id = uuid.uuid1()
+        put_run_response = _runs_table.put_item(
+            Item={
+                'run_id': new_run_id,
+                'username': username,
+                'start_time': start_time
+            }
+        )
+        print(put_run_response, f'run inserted {new_run_id}')
+        return put_run_response
