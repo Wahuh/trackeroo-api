c0514a5f84724a8330ea92470b610cc117db8838
Tim <44656173+timotillydo@users.noreply.github.com>
2019-10-05 16:34:19 +0100

    route for posting a new run event

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from chalice import Chalice, Response, BadRequestError, ChaliceViewError
 from chalicelib.auth import signup, authorizer, login
+from chalicelib.runs import add_run
 
 app = Chalice(app_name="trackeroo-api")
 
@@ -41,5 +42,22 @@ def post_login():
         )
     except KeyError:
         raise BadRequestError("Username or password is missing")
+    except Exception as e:
+        raise ChaliceViewError(e)
+
+
+@app.route("/runs", cors=True, methods=["POST"])
+def post_run():
+    try:
+        body = app.current_request.json_body
+        username = body["username"]
+        start_time = body["start_time"]
+        run = add_run(username, start_time)
+        return Response(
+            body={"run": run},
+            status_code=201,
+        )
+    except KeyError:
+        raise BadRequestError("Username or start_time is missing")
     except Exception as e:
         raise ChaliceViewError(e)
