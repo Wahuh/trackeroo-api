820933ee2e39dafd8c18657fa504e0ce409f0577
Thanh <tmdoan98@gmail.com>
2019-10-07 11:55:03 +0100

    add websocket message handler

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from chalice import Chalice, Response, BadRequestError, ChaliceViewError
+from boto3.session import Session
+from chalice import (
+    Chalice,
+    Response,
+    BadRequestError,
+    ChaliceViewError,
+    WebsocketDisconnectedError,
+)
 from chalicelib.auth import signup, authorizer, login
+from chalicelib.users import connect
+from chalicelib.runs import add_run
+import json
 
 app = Chalice(app_name="trackeroo-api")
+app.experimental_feature_flags.update(["WEBSOCKETS"])
+app.websocket_api.session = Session()
 
 
 @app.route("/", cors=True, methods=["GET"], authorizer=authorizer)
@@ -43,3 +55,44 @@ def post_login():
         raise BadRequestError("Username or password is missing")
     except Exception as e:
         raise ChaliceViewError(e)
+
+
+@app.route("/runs", cors=True, methods=["POST"])
+def post_run():
+    try:
+        body = app.current_request.json_body
+        username = body["username"]
+        start_time = body["start_time"]
+        add_run(username=username, start_time=start_time)
+    except KeyError:
+        raise BadRequestError("username and start_time must be valid")
+    except Exception as e:
+        raise ChaliceViewError(e)
+
+
+# @app.on_ws_connect()
+# def handle_connect(event):
+#     event.connection_id
+#     # get username
+
+
+@app.on_ws_message()
+def handle_message(event):
+    try:
+        # message_type = event.json_body["type"]
+        username = event.json_body["username"]
+        app.websocket_api.send(
+            event.connection_id, json.dumps({"username": username})
+        )
+    except WebsocketDisconnectedError as e:
+        pass
+
+
+# @app.on_ws_disconnect()
+# def handle_disconnect():
+#     print(5)
+
+
+# @app.lambda_function(name="runs_stream_handler")
+# def push_runs(event, context):
+#     print(event)
