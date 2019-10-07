9e4061b88a4b2621edf252c2c48dec89975ee5d0
Tim <44656173+timotillydo@users.noreply.github.com>
2019-10-07 16:41:10 +0100

    update user subscription list model

--- chalicelib/users.py
+++ chalicelib/users.py
@@ -1,8 +1,8 @@
 from .models import User
 
 
-def add_user(username, first_name, last_name, age, height, weight):
-    return User.add_one(username, first_name, last_name, age, height, weight)
+def add_user(username):
+    return User.add_one(username)
 
 
 def get_users(start_username):
@@ -11,3 +11,7 @@ def get_users(start_username):
 
 def add_follower(username, follower):
     return User.add_follower(username, follower)
+
+
+def add_subscription(username, subscription):
+    return User.add_subscription(username, subscription)
