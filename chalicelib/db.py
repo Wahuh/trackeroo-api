48b1aba04e8c309ea97e9a7b8445fb1303f53bde
Thanh <tmdoan98@gmail.com>
2019-10-07 11:56:13 +0100

    add dynamoDB streams connection and change endpoint url

--- chalicelib/db.py
+++ chalicelib/db.py
@@ -2,5 +2,6 @@ import boto3
 import os
 
 database_url = os.environ.get("DATABASE_URL")
-
-connection = boto3.resource("dynamodb", endpoint_url=database_url)
+region_name = os.environ.get("REGION_NAME")
+connection = boto3.resource("dynamodb", region_name=region_name)
+streams_connection = boto3.client("dynamodbstreams")
