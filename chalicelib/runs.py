eae7c65689c5ccbb5967447b704d64e38cfbdf2c
Tim <44656173+timotillydo@users.noreply.github.com>
2019-10-07 15:27:53 +0100

    refactored update_run parameters

--- chalicelib/runs.py
+++ chalicelib/runs.py
@@ -5,5 +5,5 @@ def add_run(username, start_time):
     return Run.add_one(username, start_time)
 
 
-def update_run(username, run_id, finish_time, average_speed, altitude, total_distance, time_taken):
-    return Run.update_one(username, run_id, finish_time, average_speed, altitude, total_distance, time_taken)
+def update_run(username, run_id, finish_time, average_speed, total_distance):
+    return Run.update_one(username, run_id, finish_time, average_speed, total_distance)
