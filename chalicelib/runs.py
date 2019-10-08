4639ba70cfac2327861567739f1f91f1e605f110
Tim <44656173+timotillydo@users.noreply.github.com>
2019-10-08 19:08:05 +0100

    get_user runs imported

--- chalicelib/runs.py
+++ chalicelib/runs.py
@@ -11,3 +11,8 @@ def update_run(username, run_id, finish_time, average_speed, total_distance, coo
 
 def get_runs_by_subscriptions(subscriptions):
     return Run.get_runs_by_subscriptions(subscriptions)
+
+
+def get_users_runs(username):
+    return Run.get_users_runs(username)
+
